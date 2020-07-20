51f09bc747b021c722173be1e7e632ee09a5e3a9
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-20 09:25:17 -0700

    added movie posters with base_url and styled with flex

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -7,15 +10,27 @@ function Row({ title, fetchUrl }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* several row_poster(s) */}
+
+        {movies.map(movie => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
     </div>
   )
 }
