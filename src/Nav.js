fce02294b0ad8168fd5378886f6c7d3fdc4ca272
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-24 12:41:33 -0700

    added Nav component and styoled it

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 
 function Nav() {
-  return (
-    <div>
+  const [show, handleShow] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false)
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav-logo"
+        src="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c529.png"
+        alt="Neflix Logo"
+      />
+      <img 
+        className="nav-avatar"
+        src="https://i.pinimg.com/originals/34/62/d2/3462d27440aa255b1c314ff16f4032b4.png"
+        alt="Netflix Avatar"
+      />
     </div>
   );
 }
